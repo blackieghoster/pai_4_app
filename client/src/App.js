e90abeb9c1a5921ca001e0a30f61edabdefacb80
blackieghoster <blackieghoster@gmail.com>
2023-06-17 02:39:57 +0200

    Exercise

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//function App() {
+//  const [data, setData] = useState(null);
+//
+//  useEffect(() => {
+//    fetch("/api")
+//      .then((res) => res.json())
+//      .then((data) => setData(data.msg));
+//  }, []);
+//
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>{data}</p>
+//      </header>
+//    </div>
+//  );
+//}
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
